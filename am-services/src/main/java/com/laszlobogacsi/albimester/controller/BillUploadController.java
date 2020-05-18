08136c890729acef7f26807ff630a2fe864aa4af
Laszlo Bogacsi <laszlo.bogacsi@itv.com>
2020-05-18 19:35:04 +0100

    restcontroller

--- am-services/src/main/java/com/laszlobogacsi/albimester/controller/BillUploadController.java
+++ am-services/src/main/java/com/laszlobogacsi/albimester/controller/BillUploadController.java
@@ -1,8 +1,6 @@
 package com.laszlobogacsi.albimester.controller;
 
-import com.laszlobogacsi.albimester.bills.elmu.ElmuBill;
 import com.laszlobogacsi.albimester.parser.BillParserFactory;
-import com.laszlobogacsi.albimester.parser.ElmuBillParser;
 import com.laszlobogacsi.albimester.parser.XMLParserService;
 import com.laszlobogacsi.albimester.persistance.BillData;
 import com.laszlobogacsi.albimester.persistance.BillTransformer;
@@ -10,15 +8,17 @@ import com.laszlobogacsi.albimester.persistance.BillType;
 import com.laszlobogacsi.albimester.upload.BillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-@Controller
+@RestController
 public class BillUploadController {
     @Autowired
     BillService billService;
@@ -29,7 +29,7 @@ public class BillUploadController {
     public ResponseEntity<Object> uploadBill(@RequestParam(value = "data") List<MultipartFile> files, @RequestParam(value = "type") BillType billType) {
         files.forEach(file -> System.out.println(file.getOriginalFilename()));
         files.forEach(file -> {
-            try(final InputStream inputStream = file.getInputStream()) {
+            try (final InputStream inputStream = file.getInputStream()) {
                 billService.save(new BillData(billType, BillTransformer.transformToJson(parserService.parseInput(inputStream, BillParserFactory.getParser(billType)))));
             } catch (IOException e) {
                 e.printStackTrace();
